37a9d5695e6fa7f29a08e3cfc2c9e09b5ceffe6b
Barnabas Ifebude <cibsmarte@gmail.com>
2022-03-09 17:17:28 +0100

    Initial Commit

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+</head>
+<body>
+    <article>
+        <h1>My First Blog Post</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus voluptatum facere similique excepturi tempora vel dolorem quos placeat repudiandae cumque? Facere temporibus esse, reprehenderit nihil earum quisquam pariatur nisi voluptatum.</p>    
+    </article>
+
+    <a href="/">Go Back</a>
+</body>
+</html>
---
+++ resources/views/blogs.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+
+    <link rel="stylesheet" href="/blog.css">
+</head>
+<body>
+    <article>
+        <h1><a href="/blog">My First Blog Post </a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus voluptatum facere similique excepturi tempora vel dolorem quos placeat repudiandae cumque? Facere temporibus esse, reprehenderit nihil earum quisquam pariatur nisi voluptatum.</p>    
+    </article>
+
+    <article>
+        <h1><a href="/blog">My Second Blog Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus voluptatum facere similique excepturi tempora vel dolorem quos placeat repudiandae cumque? Facere temporibus esse, reprehenderit nihil earum quisquam pariatur nisi voluptatum.</p>    
+    </article>
+
+    <article>
+        <h1><a href="/blog">My Third Blog Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus voluptatum facere similique excepturi tempora vel dolorem quos placeat repudiandae cumque? Facere temporibus esse, reprehenderit nihil earum quisquam pariatur nisi voluptatum.</p>    
+    </article>
+
+    <article>
+        <h1><a href="/blog">My Fourth Blog Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus voluptatum facere similique excepturi tempora vel dolorem quos placeat repudiandae cumque? Facere temporibus esse, reprehenderit nihil earum quisquam pariatur nisi voluptatum.</p>    
+    </article>
+
+    <article>
+        <h1><a href="/blog">My Fifth Blog Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus voluptatum facere similique excepturi tempora vel dolorem quos placeat repudiandae cumque? Facere temporibus esse, reprehenderit nihil earum quisquam pariatur nisi voluptatum.</p>    
+    </article>
+    
+</body>
+</html>
