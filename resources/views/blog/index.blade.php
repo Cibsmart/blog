728c3eb175a0faa48898306be96c6dd9fee78dbd
Barnabas Ifebude <cibsmarte@gmail.com>
2022-04-06 20:11:16 +0100

    Updated CSS

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,23 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog</title>
-</head>
-<body>
-    @auth
-        <a href="/blog/create">Create Post</a>
-    @endauth
+<x-app-layout>
 
-    @foreach($posts as $post)
-        <article>
-            <h1><a href="/blog/{{ $post->slug }}">{{ $post->title }}</a></h1>
-            <span>Author: {{ $post->author->name }}</span>
-            <p>{{ $post->excerpt }}</p>    
-        </article>
-    @endforeach
-    
-</body>
-</html>
+    <x-slot name="title">Blog Posts</x-slot>
+
+    <x-slot name="header">
+        Blog Posts
+    </x-slot>
+
+    <div class="mx-auto max-w-lg">
+        @auth
+            <a href="/blog/create">Create Post</a>
+        @endauth
+
+        @foreach($posts as $post)
+            <article>
+                <h1><a href="/blog/{{ $post->slug }}">{{ $post->title }}</a></h1>
+                <span>Author: {{ $post->author->name }}</span>
+                <p>{{ $post->excerpt }}</p>    
+            </article>
+        @endforeach
+    </div>
+</x-app-layout>
