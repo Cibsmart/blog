abb03ac774be4575962113327e80cb9a8ad563bd
Barnabas Ifebude <cibsmarte@gmail.com>
2022-05-30 18:38:41 +0100

    Displayed Tags from the DB

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -13,4 +14,28 @@ class Tag extends Model
     {
         return $this->belongsToMany(Blog::class);
     }
+
+    public function getColorClassesAttribute() 
+    {
+        $colors = [
+            'green' => 'bg-green-100 text-green-800',
+            'yellow' => 'bg-yellow-100 text-yellow-800',
+            'red' => 'bg-red-100 text-red-800',
+            'blue' => 'bg-blue-100 text-blue-800',
+        ];
+
+        return $colors[$this->color] ?? 'bg-gray-100 text-gray-800';
+    }
+
+    // public function colorClasses() : Attribute
+    // {
+    //     return Attribute::make(
+    //         get: fn ($value) => [
+    //                 'green' => 'bg-green-100 text-green-800',
+    //                 'yellow' => 'bg-yellow-100 text-yellow-800',
+    //                 'red' => 'bg-red-100 text-red-800',
+    //                 'blue' => 'bg-blue-100 text-blue-800',
+    //             ][$value] ?? 'bg-gray-100 text-gray-800'
+    //     );
+    // }
 }
--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -24,12 +24,13 @@
 
                 <div class="text-xs font-bold text-gray-500">
                     Tags: 
-                    <span class="inline-flex items-center px-2 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800">tech</span>
-                    <span class="inline-flex items-center px-2 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">php</span>
-                    <span class="inline-flex items-center px-2 py-0.5 rounded-full text-xs font-medium bg-red-100 text-red-800">laravel</span>
-                    <span class="inline-flex items-center px-2 py-0.5 rounded-full text-xs font-medium bg-yellow-100 text-yellow-800">javascript</span>
+                    @foreach($post->tags as $tag)
+                        <span class="inline-flex items-center px-2 py-0.5 rounded-full text-xs font-medium {{ $tag->color_classes }}">
+                            {{ $tag->name }}
+                        </span>
+                    @endforeach
                 </div>
-
+ 
                 <p>{{ $post->excerpt }}</p>    
             </article>
         @endforeach
